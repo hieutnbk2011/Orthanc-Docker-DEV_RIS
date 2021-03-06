394ace143435aba69863b9edfb3d50a889c22cb9
Stephen Douglas Scotti <sscotti@sscotti.org>
2021-03-06 17:17:43 -0600

    gitignore changes plus build changes
    Seems that if you make a fresh build and run php artisan storage:link from bin/bash then the link works.  Still might need to debug the tls cert copying.

--- tls/copy-tls-to-docker-volumes.sh
+++ tls/copy-tls-to-docker-volumes.sh
@@ -7,8 +7,8 @@ function writeFileToDockerVolume {
 }
 # copy server certs and private keys to volumes used by nginx to implement the TLS layer in front of Orthanc
 
-writeFileToDockerVolume nginx nginx-crt.pem < nginx-crt.pem
-writeFileToDockerVolume nginx nginx-key.pem < nginx-key.pem
+writeFileToDockerVolume nginx:alpine nginx-crt.pem < nginx-crt.pem
+writeFileToDockerVolume nginx:alpine nginx-key.pem < nginx-key.pem
 
 # copy root CA to orthanc-a so it accepts the self signed certificate of nginx when connecting to the orthanc-b peer
 writeFileToDockerVolume tls-with-nginx_pacs-1 ca-crt.pem < ca-crt.pem
